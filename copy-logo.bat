f7422020879066d7c7b84dc65f607641998de61b
Daniel <daniel@thermowerk.ch>
2026-03-23 06:26:57 +0100

    Update logo and add subtle overlay styling

---
+++ copy-logo.bat
@@ -0,0 +1,5 @@
+@echo off
+copy /y "C:\Users\Daniel\Downloads\Logo Thermowerk.png" "C:\Users\Daniel\Documents\thermowerk-website\public\img\logo.png"
+copy /y "C:\Users\Daniel\Downloads\Logo Thermowerk.png" "C:\Users\Daniel\Documents\thermowerk-website\img\logo.png"
+echo Logo kopiert!
+pause
