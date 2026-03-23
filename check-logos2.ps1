c6936f604a34ec67c3ec0472d88b35d10a66046c
Daniel <daniel@thermowerk.ch>
2026-03-23 06:44:12 +0100

    Fix: use correct new Thermowerk logo

---
+++ check-logos2.ps1
@@ -0,0 +1,2 @@
+Write-Host "img/logo.png:        $((Get-Item 'C:\Users\Daniel\Documents\thermowerk-website\img\logo.png').Length) bytes"
+Write-Host "public/img/logo.png: $((Get-Item 'C:\Users\Daniel\Documents\thermowerk-website\public\img\logo.png').Length) bytes"
