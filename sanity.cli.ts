e5c527ddb384547e218a31d8f02a0c879a5b178a
Daniel <daniel@thermowerk.ch>
2026-03-28 09:25:48 +0100

    Sanity Studio deployed auf thermowerk.sanity.studio, CLI config + Dependencies

---
+++ sanity.cli.ts
@@ -0,0 +1,12 @@
+import { defineCliConfig } from 'sanity/cli';
+
+export default defineCliConfig({
+  api: {
+    projectId: 'wpbatz1m',
+    dataset: 'production',
+  },
+  studioHost: 'thermowerk',
+  deployment: {
+    appId: 'qwt8h1awb60c9n1jzijqwktc',
+  },
+});
